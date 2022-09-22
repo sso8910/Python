4731929686785012687771612188ec6e16d9c960
sso8910 <dbsthwjd89@gmail.com>
2022-09-22 18:30:10 +0900

    cc

---
+++ module_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### 라이브러리(모듈)란?
+# - 모듈이라고 불리며, 자주쓰는 함수나 변수들을 모아 놓은 파일
+# - 자주 사용하는 기능을 모듈화 , 패키지화 하여 만들어둔 것
+# - 표준라이브러리 : 파이썬에서 기본으로 제공하는 라이브러리 ex)random
+# - 외부 라이브러리 : 개발자가 필요에 의해 만들어서 제공하는 라이브러리 
+
+# In[ ]:
+
+
+def add(num1,num2):
+    return num1+num2
+
+
+# In[ ]:
+
+
+def minus(num1,num2):
+    return num1-num2
+
+
+# In[ ]:
+
+
+def gob(num1,num2):
+    return num1*num2
+
+
+# In[ ]:
+
+
+def nanu(num1,num2):
+    return num1%num2
+
+
+# #### Numpy 라이브러리
